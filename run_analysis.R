1d34425ceaddc0f4023bcfbce3771cea9151f6b0
Mark Long <mark14226@gmail.com>
2016-04-24 22:36:43 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # FINAL ASSIGNMENT #
 
-# Downloaded data for assignmenet and put into new directory
+# Download data for assignmenet and put into working directory
 # Data are available at - https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 # 1. Merges the training and the test sets to create one data set.
